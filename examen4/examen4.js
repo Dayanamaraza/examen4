8a80148549ce980c757d5b822dde70cb93964309
Dayanamaraza <89095561+Dayanamaraza@users.noreply.github.com>
2021-12-14 17:21:10 +0000

    examen4
    examen4

---
+++ examen4/examen4.js
@@ -0,0 +1,45 @@
+let musican1 = document.getElementById("musican1");
+let play_btn1 = document.getElementById("play-btn1");
+let musican2 = document.getElementById("musican2");
+let play_btn2 = document.getElementById("play-btn2");
+let musican3 = document.getElementById("musican3");
+let play_btn3 = document.getElementById("play-btn3");
+let musican4 = document.getElementById("musican4");
+let play_btn4 = document.getElementById("play-btn4");
+let musican5 = document.getElementById("musican5");
+let play_btn5 = document.getElementById("play-btn5");
+let musican6 = document.getElementById("musican6");
+let play_btn6 = document.getElementById("play-btn6");
+let musican7 = document.getElementById("musican7");
+let play_btn7 = document.getElementById("play-btn7");
+let musican8 = document.getElementById("musican8");
+let play_btn8 = document.getElementById("play-btn8");
+let musican9 = document.getElementById("musican9");
+let play_btn9 = document.getElementById("play-btn9");
+ play_btn.addEventListener("click", () =>{
+     musican1.play();
+  } );
+  play_btn2.addEventListener("click", () =>{
+    musican2.play();
+ } );
+ play_btn3.addEventListener("click", () =>{
+    musican3.play();
+ } );
+ play_btn4.addEventListener("click", () =>{
+    musican4.play();
+ } );
+ play_btn5.addEventListener("click", () =>{
+    musican5.play();
+ } );
+ play_btn6.addEventListener("click", () =>{
+    musican6.play();
+ } );
+ play_btn7.addEventListener("click", () =>{
+    musican7.play();
+ } );
+ play_btn8.addEventListener("click", () =>{
+    musican8.play();
+ } );
+ play_btn9.addEventListener("click", () =>{
+    musican9.play();
+ } );
